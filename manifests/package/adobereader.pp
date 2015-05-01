18fdf350f458e5a59b84ce1b6a508ff30f176078
J Harrington <ultra_jason@yahoo.com>
2015-04-30 23:17:07 -0500

    initial commit of my adobe puppet module

---
+++ manifests/package/adobereader.pp
@@ -0,0 +1,63 @@
+# == Class: adobe::package::adobereader
+#
+# Define pared down Adobe repo.
+# Install AdobeReader_enu (i386, as x86_64 does not exist)
+#
+# === Parameters
+#
+# None
+#
+# === Variables
+#
+# None
+#
+# === Examples
+#
+# class { 'adobe::package::adobereader': }
+#
+# include adobe::package::adobereader
+#
+# === Authors
+#
+# Jason Harrington <jason@fnal.gov>
+#
+# === Copyright
+#
+# No copyright expressed, or implied.
+#
+class adobe::package::adobereader {
+
+    # there is some amount of annoyance here
+    # there is no 64-bit package so only install 32-bit
+
+    # need Adobe repo GPG key, but could be present from other pacakges
+    if ! defined(File['/etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux']) {
+        file {'/etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux':
+            ensure => 'file',
+            source => 'puppet:///modules/yum/gpgkeys/RPM-GPG-KEY-adobe-linux',
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0644',
+        }
+    }
+
+    # define limited version of Adobe repo which will only bring in AdobeReader
+    yumrepo {'adobereader-i386':
+        name           => 'adobereader-i386',
+        descr          => 'AdobeReader package from Adobe',
+        baseurl        => 'http://linuxdownload.adobe.com/linux/i386/',
+        failovermethod => 'priority',
+        enabled        => '1',
+        gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux',
+        gpgcheck       => '1',
+        includepkgs    => 'AdobeReader*',
+        require        => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux'],
+    }
+
+    # install the package
+    package {'AdobeReader_enu':
+        ensure  => 'latest',
+        require => Yumrepo['adobereader-i386'],
+    }
+
+}
---
+++ manifests/package/flash_plugin.pp
@@ -0,0 +1,68 @@
+# == Class: adobe::package::flash_plugin
+#
+# Define pared down Adobe repo.
+# Install flash-plugin.
+#
+# === Parameters
+#
+# None
+#
+# === Variables
+#
+# None
+#
+# === Examples
+#
+# class { 'adobe::package::flash_plugin': }
+#
+# include adobe::package::flash_plugin
+#
+# === Authors
+#
+# Jason Harrington <jason@fnal.gov>
+#
+# === Copyright
+#
+# No copyright expressed, or implied.
+#
+class adobe::package::flash_plugin {
+
+    # there is some amount of annoyance here
+    # only i386 works reliably on SLF5, but we follow arch on SLF6
+    $reliable_arch = $::lsbmajdistrelease ? {
+        '5' => 'i386',
+        '6' => $::architecture,
+        '7' => $::architecture,
+    }
+
+    # need Adobe repo GPG key, but could be present from other packages
+    if ! defined(File['/etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux']) {
+        file {'/etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux':
+            ensure => 'file',
+            source => 'puppet:///modules/yum/gpgkeys/RPM-GPG-KEY-adobe-linux',
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0644',
+        }
+    }
+
+    # define limited version of Adobe repo which will only bring in flash-plugin
+    yumrepo {"flash-plugin-${reliable_arch}":
+        name           => "flash-plugin-${reliable_arch}",
+        descr          => 'flash-plugin package from Adobe',
+        baseurl        => "http://linuxdownload.adobe.com/linux/${reliable_arch}/",
+        failovermethod => 'priority',
+        enabled        => '1',
+        gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux',
+        gpgcheck       => '1',
+        includepkgs    => 'flash-plugin',
+        require        => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux'],
+    }
+
+    # install the package
+    package {'flash-plugin':
+        ensure  => 'installed',
+        require => Yumrepo["flash-plugin-${reliable_arch}"],
+    }
+
+}
